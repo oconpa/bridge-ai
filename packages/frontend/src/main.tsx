be4bd9f31e839cd60210265ecb929673768b49e6
Patrick <ocopatr@amazon.com>
2025-05-17 15:31:53 -0700

    update

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,20 +1,27 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { awsconfig } from "./aws-config.ts";
-import { router } from "./routes.tsx";
 import { Amplify } from "aws-amplify";
+import { router } from "./routes.tsx";
 import { StrictMode } from "react";
 
 import "@cloudscape-design/global-styles/index.css";
 import "@aws-amplify/ui-react/styles.css";
+
+const queryClient = new QueryClient();
 
 Amplify.configure(awsconfig);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Authenticator hideSignUp variation="modal">
-      {() => <RouterProvider router={router} />}
+      {() => (
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      )}
     </Authenticator>
   </StrictMode>
 );
--- packages/frontend/src/pages/Delegate.tsx
+++ packages/frontend/src/pages/Delegate.tsx
@@ -1,17 +1,51 @@
 import { FileUploader } from "@aws-amplify/ui-react-storage";
+import { useListS3 } from "../api/query";
+import { useState } from "react";
 
 import {
+  Button,
   Container,
   ContentLayout,
+  Form,
+  FormField,
   Header,
+  Select,
+  SpaceBetween,
 } from "@cloudscape-design/components";
 
 export const Delegate = () => {
+  const list = useListS3("");
+
+  const [selectedOption, setSelectedOption] = useState({});
+
   return (
     <ContentLayout header={<Header variant="h1">Delegate</Header>}>
-      <Container header={<Header variant="h2">Upload Spec</Header>}>
-        <FileUploader path="" maxFileCount={1} />
-      </Container>
+      <SpaceBetween size="l">
+        <Container header={<Header variant="h2">Upload Spec</Header>}>
+          <FileUploader path="" maxFileCount={1} />
+        </Container>
+
+        <Container header={<Header variant="h2">Delegate</Header>}>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <Form actions={<Button variant="primary">Submit</Button>}>
+              <SpaceBetween direction="vertical" size="l">
+                <FormField label="First field">
+                  <Select
+                    selectedOption={selectedOption}
+                    onChange={({ detail }) =>
+                      setSelectedOption(detail.selectedOption)
+                    }
+                    options={list.data?.items.map((item) => ({
+                      label: item.path,
+                      value: item.path,
+                    }))}
+                  />
+                </FormField>
+              </SpaceBetween>
+            </Form>
+          </form>
+        </Container>
+      </SpaceBetween>
     </ContentLayout>
   );
 };
